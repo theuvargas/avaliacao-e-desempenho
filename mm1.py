d8ff5e0b33af0e5fa2ce75f9885159227e157d86
theuvargas <matheusvargas412@gmail.com>
2024-07-17 19:57:24 -0300

    primeiro cliente chega no tempo 0

--- mm1.py
+++ mm1.py
@@ -10,8 +10,12 @@ def main():
     l = 0.5
     mu = 1
 
-    tempo_no_sistema = 0
-    tempos = [0] * n # timestamp em que cada servidor ficará ocioso
+    tempos = [0] * n # timestamp a partir do qual cada servidor ficará ocioso
+
+    # o primeiro cliente chega no tempo 0
+    servico_primeiro_cliente = exponencial(mu)
+    tempos[randint(0, n-1)] = servico_primeiro_cliente
+    tempo_no_sistema = servico_primeiro_cliente
 
     t = 0
     i = 0
@@ -31,7 +35,7 @@ def main():
 
         i += 1
     
-    print(tempo_no_sistema/i)
+    print(f"Tempo médio no sistema: {tempo_no_sistema/i}s")
 
 
 if __name__ == "__main__":
